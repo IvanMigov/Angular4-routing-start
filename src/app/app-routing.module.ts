3dc16e6defee04fa582d3728de695850f424e8b0
ivan <ivan.migov@gmail.com>
2017-05-07 15:55:41 +0300

    138

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,15 +10,20 @@ import {UserComponent} from "./users/user/user.component";
 import {UsersComponent} from "./users/users.component";
 import {HomeComponent} from "./home/home.component";
 import {AuthGuard} from "./auth-guard.service";
+import {CanDeactivateGuard} from "./servers/edit-server/can-deactivate-guard.service";
 
 const appRoutes:Routes = [
   {path:'', component: HomeComponent},
   {path:'users', component: UsersComponent,children:[
     {path:':id/:name', component: UserComponent},
   ]},
-  {path:'servers',canActivate: [AuthGuard], component: ServersComponent, children:[
+  {path:'servers',
+    // canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    component: ServersComponent,
+    children:[
     {path:':id', component: ServerComponent},
-    {path:':id/edit', component: EditServerComponent},
+    {path:':id/edit', component: EditServerComponent,canDeactivate:[CanDeactivateGuard]},
   ]},
   {path:'not-found', component: PageNotFoundComponent},
   {path:'**', redirectTo:'/not-found'},
--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,12 +1,15 @@
-import {CanActivate,
+import {
+  CanActivate,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
-  Router} from "@angular/router";
+  Router,
+  CanActivateChild
+} from "@angular/router";
 import {Observable} from "rxjs/Observable";
 import {Injectable} from "@angular/core";
 import {AuthService} from "./auth.service";
 @Injectable()
-export  class AuthGuard implements CanActivate{
+export  class AuthGuard implements CanActivate,CanActivateChild{
   constructor(private authService: AuthService,
               private router: Router) {
   }
@@ -22,5 +25,10 @@ export  class AuthGuard implements CanActivate{
           }
         }
       );
+  }
+  canActivateChild(route: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot):Observable<boolean>|Promise<boolean>|boolean {
+
+    return this.canActivate(route,state);
   }
 }
